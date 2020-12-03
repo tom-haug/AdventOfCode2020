e789ace1eb89db599e5a21a327ae572bcf2a2a89
Tom Haug <tomhaug75@gmail.com>
2020-12-02 19:35:29 -0500

    day01

---
+++ app.py
@@ -0,0 +1,9 @@
+def sum_numbers(num1, num2):
+    result = num1 + num2
+    return result
+
+num1 = float(input("First: "))
+num2 = float(input("Second: "))
+
+result = sum_numbers(num1, num2)
+print(result)
